9267ba0dc299f490560848013f7eeb22325154fd
parro-it <parro-it@users.noreply.github.com>
2021-04-19 09:27:46 +0000

    Initialization

--- examples_test.go
+++ examples_test.go
@@ -1,19 +1,19 @@
-package {{.RepoName}}_test
+package horzmerge_test
 
 import (
 	"embed"
 	"fmt"
 	"io/fs"
 	
-	"github.com/{{.Author}}/{{.RepoName}}"
+	"github.com/parrogo/horzmerge"
 )
 
 //go:embed fixtures
 var fixtureRootFS embed.FS
 var fixtureFS, _ = fs.Sub(fixtureRootFS, "fixtures")
 
-// This example show how to use {{.RepoName}}.Func()
+// This example show how to use horzmerge.Func()
 func ExampleFunc() {
-	fmt.Println({{.RepoName}}.Func())
+	fmt.Println(horzmerge.Func())
 	// Output: 42
 }
---
+++ horzmerge.go
@@ -0,0 +1,12 @@
+// Package horzmerge merges columns from one or more streams of data.
+package horzmerge
+
+// # TODO: remove or rename `horzmerge.Func` function
+
+// # TODO: configure issues & PR in Code Climate
+// # You can configure settings [here](https://codeclimate.com/repos/607d4cfb0d15b5444300820c/edit)
+
+// Func answers
+func Func() int {
+	return 42
+}
